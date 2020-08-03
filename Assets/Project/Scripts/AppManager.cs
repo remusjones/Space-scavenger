6f73c3e23dae9e6e2298867a96953e88f4c62f40
John Dunning <john.charles.dunning@gmail.com>
2020-08-03 10:40:31 +1000

    Added appmanager & spawnpoint

---
+++ Assets/Project/Scripts/AppManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppManager : MonoBehaviour
+{
+    public GameObject _PlayerPrefab;
+    public Transform _PlayerSpawn;
+
+    private void Awake()
+    {
+        
+    }
+}
