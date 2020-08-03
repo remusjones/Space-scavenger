b0d2c23301b601db72491a47e97fba079fcf8e3a
Remus Jones <remus.jones@hotmail.com>
2020-08-03 10:59:58 +1000

    Added Docking class and IDock internface.
    Added Docking class and IDock internface.

---
+++ Assets/Project/Scripts/Interface/IDock.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+interface IDock
+{
+    bool CanDock(Vector3 location, float checkDistance, Transform closestDock);
+    void InitateDock(Transform ourObject, Transform otherObject);
+}
