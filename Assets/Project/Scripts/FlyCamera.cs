00cc2f64e9fb8e31f9b372a11159bb72dd667736
John Dunning <john.charles.dunning@gmail.com>
2020-08-03 14:24:43 +1000

    WIP Interior Ship

--- Assets/Project/Scripts/FlyCamera.cs
+++ Assets/Project/Scripts/FlyCamera.cs
@@ -20,9 +20,15 @@ public class FlyCamera : MonoBehaviour
     public float camSens = 0.25f; //How sensitive it with mouse
     private Vector3 lastMouse = new Vector3(255, 255, 255); //kind of in the middle of the screen, rather than at the top (play)
     private float totalRun = 1.0f;
+    private void Start()
+    {
+        
+        Cursor.visible = false;
+    }
 
     void Update()
     {
+        
         lastMouse = Input.mousePosition - lastMouse;
         lastMouse = new Vector3(-lastMouse.y * camSens, lastMouse.x * camSens, 0);
         lastMouse = new Vector3(transform.eulerAngles.x + lastMouse.x, transform.eulerAngles.y + lastMouse.y, 0);
--- Assets/Project/Scripts/PlayerController.cs
+++ Assets/Project/Scripts/PlayerController.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class PlayerController : MonoBehaviour
 {
     public float _InteractRange;
     public LayerMask _InteractLayer;
 
-
+    public Image _InteractMarker;
+    public Image _DefaultMarker;
     // Update is called once per frame
     void Update()
     {
@@ -16,8 +17,13 @@ public class PlayerController : MonoBehaviour
         {
             if (hit.collider.GetComponent<IObjectInteract>() != null)
             {
+                _InteractMarker.enabled = true;
                 if(MouseDown())
                     hit.collider.GetComponent<IObjectInteract>().Interact();
+            }
+            else
+            {
+                _InteractMarker.enabled = false;
             }
         }
         Debug.DrawRay(transform.position, transform.forward);
