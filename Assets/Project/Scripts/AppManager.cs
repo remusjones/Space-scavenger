7840092aa8258cf5f408e581986c64f5a206c826
John Dunning <john.charles.dunning@gmail.com>
2020-08-03 10:54:06 +1000

    Added basic temp flycam to the player prefab

--- Assets/Project/Scripts/AppManager.cs
+++ Assets/Project/Scripts/AppManager.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class AppManager : MonoBehaviour
 {
     public GameObject _PlayerPrefab;
+    [HideInInspector]
+    public GameObject _PlayerInstance;
     public Transform _PlayerSpawn;
 
     private void Awake()
     {
-        
+        Init();
+    }
+    private void Init()
+    {
+        _PlayerInstance = Instantiate(_PlayerPrefab);
+        _PlayerInstance.transform.SetPositionAndRotation(_PlayerSpawn.position, _PlayerSpawn.rotation);
     }
 }
