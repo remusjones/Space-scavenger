01ad2c94a18f60c06d76bb01b554d1ed90384183
Remus Jones <remus.jones@hotmail.com>
2020-08-05 15:00:15 +1000

    Added a basic ship model, to use when flying. Modified ship controller to behave similarly to the player controller, just heavier.
    Added a basic ship model, to use when flying. Modified ship controller to behave similarly to the player controller, just heavier.

--- Assets/Project/Scripts/Ship.cs
+++ Assets/Project/Scripts/Ship.cs
@@ -10,7 +10,7 @@ public class Ship : MonoBehaviour
     public bool isPlayerDriving = false;
 
     // Start is called before the first frame update
-    Vector2 lastMouseFrame = new Vector2();
+    Vector2 lastMouse = new Vector2();
     Rigidbody rb = null;
 
     [SerializeField]
@@ -19,12 +19,29 @@ public class Ship : MonoBehaviour
     Vector2 currMouse;
     [SerializeField]
     Vector2 mouseInput;
+    private Vector2 residualVelocity = Vector2.zero;
 
+    private float rotX;
+
+    [SerializeField]
+    float residualMagnitudeLimit = 0.1f;
+    [SerializeField]
+    float residualDivider = 50f;
+    [SerializeField]
+    float residualDeductionMultiplier = 5f;
     [SerializeField]
     float directionAccel = 100f;
     [SerializeField]
-    float pitchAccel = 80f;
+    float pitchAccel = 100f;
 
+    [SerializeField]
+    float deaccelSpeed = 1f;
+
+
+    public float turnSpeed = 1.0f;
+    public float moveSpeed = 2.0f;
+    public float minTurnAngle = -90.0f;
+    public float maxTurnAngle = 90.0f;
 
     void Start()
     {
@@ -40,18 +57,50 @@ public class Ship : MonoBehaviour
     {
         if (!isPlayerDriving)
             return;
-        input = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("UpDown"), Input.GetAxis("Vertical"));
+
+
+        Cursor.lockState = CursorLockMode.Locked;
+        rb.angularVelocity -= rb.angularVelocity / 10;
+        float mX = Input.GetAxis("Mouse X");
+        float mY = Input.GetAxis("Mouse Y");
         float pitch = Input.GetAxis("Pitch");
-        currMouse = Input.mousePosition;
-        mouseInput = (lastMouseFrame - currMouse);
 
-
-        rb.AddRelativeTorque((new Vector3(0, pitch, 0) * pitchAccel) * Time.fixedDeltaTime, ForceMode.Force);
-        rb.AddRelativeForce((new Vector3(input.x, input.y, input.z) * directionAccel) * Time.fixedDeltaTime, ForceMode.Force);
-        lastMouseFrame = currMouse;
-        if (Input.GetKey(KeyCode.LeftAlt))
+        Vector2 currMouse = new Vector2(mX, mY);
+        if ((currMouse - lastMouse).magnitude >= residualMagnitudeLimit)
         {
-            rb.velocity = rb.velocity / 4;
+            residualVelocity = (currMouse + lastMouse) / residualDivider;
+        }
+        else
+        {
+            residualVelocity -= ((residualVelocity * residualDeductionMultiplier) * Time.deltaTime);
+        }
+
+
+        float y = ((mX + residualVelocity.x) * turnSpeed);
+        rotX = ((mY + residualVelocity.y) * turnSpeed);
+        rotX = Mathf.Clamp(rotX, minTurnAngle, maxTurnAngle);
+       // this.transform.Rotate(Vector3.up, y, Space.Self);
+       // // //rotate around local x;
+       // this.transform.Rotate(Vector3.right, -rotX, Space.Self);
+
+
+        lastMouse = currMouse;
+        // input
+
+        if (rb)
+        {
+            Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("UpDown"), Input.GetAxis("Vertical"));
+            pitch = Input.GetAxis("Pitch");
+
+            rb.AddRelativeTorque((new Vector3(0, 0, -pitch) * pitchAccel) * Time.fixedDeltaTime, ForceMode.Acceleration);
+            rb.AddRelativeForce((new Vector3(movement.x, movement.y, movement.z) * directionAccel) * Time.fixedDeltaTime, ForceMode.Force);
+            rb.AddRelativeTorque(-(residualVelocity.y), (residualVelocity.x), 0f);
+            //rb.angularVelocity = new Vector3(rb.angularVelocity.x + (residualVelocity.y / 10), rb.angularVelocity.y + (residualVelocity.x / 10), rb.angularVelocity.z);
+
+            if (Input.GetKey(KeyCode.LeftAlt))
+            {
+                rb.velocity = rb.velocity - ((rb.velocity * deaccelSpeed) * Time.deltaTime);
+            }
         }
     }
     public void EnableShipControls(bool isEnabled)
