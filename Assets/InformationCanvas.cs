cc23b807ed6780c7083552e5c3c0e6636ea3dcc7
Remus Jones <remus.jones@hotmail.com>
2020-08-06 22:12:26 +1000

    Added Helmet, added InformationCanvas
    Added Helmet, added InformationCanvas

---
+++ Assets/InformationCanvas.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InformationCanvas : MonoBehaviour
+{
+    [SerializeField]
+    TMP_Text oxygenValue = null;
+    [SerializeField]
+    TMP_Text speedValue = null;
+    private string speedAppend = "m/s";
+
+    [SerializeField]
+    Rigidbody playerRigidbody = null;
+
+    bool isRunning = false;
+    
+
+    private void Start()
+    {
+        isRunning = true;
+        StartCoroutine(UpdateInformationUI());
+    }
+    IEnumerator UpdateInformationUI()
+    {
+        while(isRunning)
+        {
+            yield return new WaitForEndOfFrame();
+
+            float playerSpeed = playerRigidbody.velocity.magnitude;
+
+            string printString;
+            if (playerSpeed <= 99.9f)
+            {
+                printString = playerSpeed.ToString("0.0")+speedAppend;
+            }else
+            {
+                printString = playerSpeed.ToString("0") + speedAppend;
+            }
+
+            speedValue.text = printString;
+        }
+    }
+}
