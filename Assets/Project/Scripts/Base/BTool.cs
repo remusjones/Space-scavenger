6b307700f83538b6a8cc0662da74a790f37676ca
Remus Jones <remus.jones@hotmail.com>
2020-08-09 19:48:38 +1000

    Added Handgun, added btool coroutines (optional)
    Added Handgun, added btool coroutines (optional)

--- Assets/Project/Scripts/Base/BTool.cs
+++ Assets/Project/Scripts/Base/BTool.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class BTool : MonoBehaviour, ITool
 {
@@ -30,6 +31,11 @@ public class BTool : MonoBehaviour, ITool
     protected float storedAmmo;
     [SerializeField]
     protected LayerMask layermask;
+    [SerializeField]
+    protected float reloadSpeed = 0f;
+    [SerializeField]
+    protected float fireRate = 0f;
+
 
     [Header("Cooldown Values")]
     [SerializeField]
@@ -37,8 +43,31 @@ public class BTool : MonoBehaviour, ITool
     [SerializeField]
     protected float reloadCooldown;
 
+    [SerializeField]
+    protected UnityEvent onShootEvent = null;
 
+    [SerializeField]
+    protected UnityEvent onReloadEvent = null;
 
+    protected bool isReloadCoroutineRunning = false;
+    protected bool isShootCoroutineRunning = false;
+
+    protected IEnumerator onShootCoroutine = null;
+    protected IEnumerator onReloadCoroutine = null;
+    protected virtual void Start()
+    {
+        onShootCoroutine = ShootCoroutine(fireRate);
+        onReloadCoroutine = ReloadWeaponCoroutine(reloadSpeed);
+    }
+
+    protected virtual void OnShootEvent()
+    {
+        onShootEvent?.Invoke();
+    }
+    protected virtual void OnReloadEvent()
+    {
+        onReloadEvent?.Invoke();
+    }
     public virtual void AddAmmo(float addAmount)
     {
         storedAmmo += addAmount;
@@ -49,8 +78,35 @@ public class BTool : MonoBehaviour, ITool
         storedAmmo -= deductAmount;
     }
 
+    public virtual IEnumerator ReloadWeaponCoroutine(float delay)
+    {
+        if (isReloadCoroutineRunning)
+            yield return null;
+
+        isReloadCoroutineRunning = true;
+        OnReloadEvent();
+        yield return new WaitForSeconds(delay);
+        Reload();
+        isReloadCoroutineRunning = false;
+
+        yield return null;
+    }
+    public virtual IEnumerator ShootCoroutine(float fireRate)
+    {
+        if (isShootCoroutineRunning)
+            yield return null;
+
+        isShootCoroutineRunning = true;
+        yield return new WaitForSeconds(fireRate);
+        OnShootEvent();
+        isShootCoroutineRunning = false;
+
+        yield return null;
+    }
     public virtual void Reload()
     {
+
+
         float diff = storedAmmo - magazineAmount;
         if (diff >= 0)
         {
@@ -86,6 +142,18 @@ public class BTool : MonoBehaviour, ITool
         raycastHit = hit;
         return false;
     }
+    public virtual bool RaycastFromToolToCamera(Transform toolNozzle, out RaycastHit raycastHit)
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(toolNozzle.position, playerCamera.transform.forward);
+        if (Physics.Raycast(ray, out hit, range, layermask))
+        {
+            raycastHit = hit;
+            return true;
+        }
+        raycastHit = hit;
+        return false;
+    }
     public bool CanShoot(float ammoCost)
     {
         if (currentMagazineAmount <= 0 || (currentMagazineAmount - ammoCost) < 0f)
@@ -100,6 +168,8 @@ public class BTool : MonoBehaviour, ITool
     /// <param name="ammoMultiplier">Leave as 0.0f if you don't want to use it</param>
     public virtual void Shoot(float damage, float ammoCost, float ammoMultiplier = 0.0f)
     {
+
+        OnShootEvent();
         if (ammoMultiplier == 0.0f)
         {
             currentMagazineAmount -= ammoCost;
@@ -110,7 +180,8 @@ public class BTool : MonoBehaviour, ITool
         }
     }
     public virtual void Shoot()
-    { 
+    {
+        OnShootEvent();
     }
 
     protected virtual void Update()
---
+++ Assets/Project/Scripts/Handgun.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Handgun : BTool
+{
+    [SerializeField]
+    private LineRenderer weaponLineRenderer = null;
+
+    protected override void Start()
+    {
+        onShootCoroutine = ShootCoroutine(fireRate);
+        onReloadCoroutine = ReloadWeaponCoroutine(reloadSpeed);
+    }
+
+    public override IEnumerator ShootCoroutine(float fireRate)
+    {
+
+        Debug.Log("Coroutine Entered.. ");
+        if (isShootCoroutineRunning || !CanShoot(1))
+            yield return null;
+
+
+        Debug.Log("Coroutine Starting.. ");
+        isShootCoroutineRunning = true;
+        OnShootEvent();
+        Shoot(damage, 1f, 0);
+        yield return new WaitForSeconds(fireRate);
+        isShootCoroutineRunning = false;
+        Debug.Log("Coroutine Ending.. ");
+        yield return null;
+    }
+    protected override void Update()
+    {
+        base.Update();
+        if (Input.GetMouseButtonDown(0))
+        {
+            ShootWeapon();
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ReloadWeapon();
+        }
+        if (!isShootCoroutineRunning)
+            weaponLineRenderer.enabled = false;
+    }
+
+    /// <summary>
+    /// Use this in input events
+    /// </summary>
+    public void ShootWeapon()
+    {
+        if (isShootCoroutineRunning)
+            return;
+        else
+        {
+            StartCoroutine(onShootCoroutine);
+        }
+    }
+    public void ReloadWeapon()
+    {
+        if (isReloadCoroutineRunning)
+            return;
+        else
+        {
+            StartCoroutine(onReloadCoroutine);
+        }
+    }
+    public override void Shoot(float damage, float ammoCost, float ammoMultiplier = 0.0f)
+    {
+
+        ApplyKnockback(playerRigidbody, playerKnockback);
+        base.Shoot(damage, ammoCost, ammoMultiplier);
+
+        // do projectile/raycast here
+        RaycastHit hit;
+        if (base.RaycastFromCamera(out hit))
+        {
+
+            Vector3[] vecs = new Vector3[2] { weaponNozzle.position, hit.point };
+            Vector3 backDir = (hit.point - weaponNozzle.position) * 0.01f;
+
+            if (weaponLineRenderer)
+            {
+                weaponLineRenderer.SetPositions(vecs);
+            }
+
+            IDamageable damageable = hit.transform.GetComponent<IDamageable>();
+            if (damageable != null)
+            {
+                damageable.Damage(damage);
+            }
+        }
+        else
+        {
+
+            Vector3[] vecs = new Vector3[2] { weaponNozzle.position, playerCamera.transform.position + (playerCamera.transform.forward * range) };
+            if (weaponLineRenderer)
+            {
+                weaponLineRenderer.SetPositions(vecs);
+            }
+        }
+    }
+
+}
