9cf55d91fc1035490899919444702ef69baa64d2
John Dunning <john.charles.dunning@gmail.com>
2020-08-12 19:26:41 +1000

    Ship aimer wip

---
+++ Assets/Project/Sandbox/ShipAimer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipAimer : MonoBehaviour
+{
+    RectTransform _Transform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _Transform = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float posX = 0,posY = 0;
+        posX += Input.GetAxis("Mouse X") * 30 * Time.deltaTime;
+        posY += Input.GetAxis("Mouse Y") * 30 * Time.deltaTime;
+
+        posX = Mathf.Clamp(posX, -100, 100);
+        posY = Mathf.Clamp(posY, -100, 100);
+        transform.position = new Vector2(posX , posY);
+    }
+}
