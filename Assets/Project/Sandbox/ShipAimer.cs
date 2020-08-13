e86613932fbf350f1e80f6445b1060699d02388f
John Dunning <john.charles.dunning@gmail.com>
2020-08-13 19:17:31 +1000

    Updated material used for prefabed ship parts

--- Assets/Project/Sandbox/ShipAimer.cs
+++ Assets/Project/Sandbox/ShipAimer.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class ShipAimer : MonoBehaviour
 {
+    public float _Speed;
     RectTransform _Transform;
+    float posX = 0, posY = 0;
+    public Ship _Ship;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,12 +17,14 @@ public class ShipAimer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float posX = 0,posY = 0;
-        posX += Input.GetAxis("Mouse X") * 30 * Time.deltaTime;
-        posY += Input.GetAxis("Mouse Y") * 30 * Time.deltaTime;
+        Vector3 AngularVelocity = Vector3.Cross(_Ship.transform.forward, _Ship.GetComponent<Rigidbody>().angularVelocity);
+        //Vector2 AngularVelocity = _Ship.GetComponent<Rigidbody>().angularVelocity;
+        posX = AngularVelocity.x * _Speed;//-Input.GetAxis("Mouse X") * _Speed * Time.deltaTime;
+        posY = AngularVelocity.y * _Speed;//-Input.GetAxis("Mouse Y") * _Speed * Time.deltaTime;
 
-        posX = Mathf.Clamp(posX, -100, 100);
-        posY = Mathf.Clamp(posY, -100, 100);
-        transform.position = new Vector2(posX , posY);
+        //posX = Mathf.Clamp(posX, -200, 200);
+        //posY = Mathf.Clamp(posY, -200, 200);
+        transform.localPosition = new Vector2(posX , posY);
+        //transform.localPosition = Input.mousePosition;
     }
 }
