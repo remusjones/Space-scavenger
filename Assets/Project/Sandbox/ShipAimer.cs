0e0dd605cab47db6adc484607ae6e402df71ba4f
Remus Jones <remus.jones@hotmail.com>
2020-08-14 10:54:14 +1000

    Fixed ShipAimer
    Fixed ShipAimer

--- Assets/Project/Sandbox/ShipAimer.cs
+++ Assets/Project/Sandbox/ShipAimer.cs
@@ -5,26 +5,14 @@ using UnityEngine;
 public class ShipAimer : MonoBehaviour
 {
     public float _Speed;
-    RectTransform _Transform;
     float posX = 0, posY = 0;
     public Ship _Ship;
-    // Start is called before the first frame update
-    void Start()
-    {
-        _Transform = GetComponent<RectTransform>();
-    }
-
     // Update is called once per frame
     void Update()
     {
-        Vector3 AngularVelocity = Vector3.Cross(_Ship.transform.forward, _Ship.GetComponent<Rigidbody>().angularVelocity);
-        //Vector2 AngularVelocity = _Ship.GetComponent<Rigidbody>().angularVelocity;
-        posX = AngularVelocity.x * _Speed;//-Input.GetAxis("Mouse X") * _Speed * Time.deltaTime;
-        posY = AngularVelocity.y * _Speed;//-Input.GetAxis("Mouse Y") * _Speed * Time.deltaTime;
-
-        //posX = Mathf.Clamp(posX, -200, 200);
-        //posY = Mathf.Clamp(posY, -200, 200);
+        Vector3 position = this.transform.InverseTransformDirection(_Ship.GetComponent<Rigidbody>().angularVelocity);
+        posX = position.y * _Speed;
+        posY = position.x * _Speed;
         transform.localPosition = new Vector2(posX , posY);
-        //transform.localPosition = Input.mousePosition;
     }
 }
