10354c4c642c7f476877dd38df14196f2e0a5a6e
John Dunning <john.charles.dunning@gmail.com>
2020-08-15 17:58:55 +1000

    Added tiling line ui thing to show y position

--- Assets/Project/Sandbox/ShipAimer.cs
+++ Assets/Project/Sandbox/ShipAimer.cs
@@ -8,6 +8,7 @@ public class ShipAimer : MonoBehaviour
     float posX = 0, posY = 0;
     public Ship _Ship;
     Rigidbody shipRigidbody = null;
+    public GameObject _PitchMat;
     private void Start()
     {
         shipRigidbody = _Ship.GetComponent<Rigidbody>();
@@ -18,6 +19,11 @@ public class ShipAimer : MonoBehaviour
         Vector3 position = this.transform.InverseTransformDirection(shipRigidbody.angularVelocity);
         posX = position.y * _Speed;
         posY = -position.x * _Speed;
+        float offset = Time.time * 10;
+        Material mat = _PitchMat.GetComponent<MeshRenderer>().material;
+        float pos = mat.GetTextureOffset("_UnlitColorMap").y;
+        pos = pos += posY;
+        mat.SetTextureOffset("_UnlitColorMap", new Vector2(0, transform.position.y /100));
         transform.localPosition = new Vector2(posX , posY);
     }
 }
